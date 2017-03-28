048051162fefd478eab3ae6ab6b8658efe6fc556
Kyuho Kim <valve.god.project@gmail.com>
2017-03-29 03:29:06 +0900

    read ds18b20 and send it to ThingSpeak

---
+++ thingspeak_ds18b20.py
@@ -0,0 +1,35 @@
+import os
+import glob
+import time
+import urllib.request
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+  f = open(device_file, 'r')
+  lines = f.readlines()
+  f.close()
+  return lines
+
+def read_temp():
+  lines = read_temp_raw()
+  while lines[0].strip()[-3:] != 'YES':
+    time.sleep(0.2)
+    lines = read_temp_raw()
+  equals_pos = lines[1].find('t=')
+  if equals_pos != -1:
+    temp_string = lines[1][equals_pos+2:]
+    temp_c = float(temp_string) / 1000.0
+    return temp_c
+
+while True:
+  t = read_temp()
+  f = urllib.request.urlopen('http://api.thingspeak.com/update?api_key=YVPZS6ZC2NJFSIY4&field1='+ str(t))
+  html = int(f.read())
+  print(html, t)
+  time.sleep(60)
